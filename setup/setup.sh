502b9bdb8a76c0c005afc4dbc1ce0ba19d39868b
Raphael Mitsch <laarem33@hotmail.com>
2017-10-27 15:49:32 +0200

    Updated .yml files.

---
+++ setup/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+## Manual setup steps. ##
+
+# Download repository and install package for multi-threaded t-SNE.
+pip install --no-cache-dir git+https://github.com/DmitryUlyanov/Multicore-TSNE.git
