d444560083626608566f1c4c987cdf8a6c9b7b52
Raphael Mitsch <laarem33@hotmail.com>
2017-10-31 17:46:53 +0100

    Proceeded work on UI design. Next: Panel for starting a new run. After that: Integration of database into existing UI; layout of dashboard; dashboard implementation.

---
+++ source/frontend/static/slider_init.js
@@ -0,0 +1,21 @@
+$(function ()
+{
+    for(var i = 1; i < 14; i++) {
+        $("#slider" + i).ionRangeSlider({
+            hide_min_max: true,
+            keyboard: true,
+            min: 0,
+            max: 5000,
+            from: 1000,
+            to: 4000,
+            type: 'single',
+            step: 1,
+            grid: true,
+            prettify_enabled: true,
+            hide_min_max: true,
+            hide_from_to: true,
+            grid_num: 3
+        });
+    }
+});
+
